7fcb96fa2bef015d279a15eb362ce319746baf0f
Kauan Meira <kauangabriel.meira123@gmail.com>
2023-03-08 10:33:00 -0300

    generic types

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,30 @@
 package application;
 
-import services.PrintService;
+import entities.Product;
+import services.CalculationService;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        PrintService <String>ps = new PrintService<>();
-        System.out.println("How many values?");
-        int n = sc.nextInt();
-        for (int i=0; i<n;i++){
-            String value = sc.next();
-            ps.addValue(value);
+        List<Product> list = new ArrayList<>();
+        String path = "C:\\Users\\Suporte-D\\IdeaProjects\\in.txt";
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+                line = br.readLine();
+            }
+            Product x = CalculationService.max(list);
+            System.out.println("Most Expensive:");
+            System.out.println(x);
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
         }
-        ps.print();
-        String x = ps.first();
-        System.out.print("First: "+ps.first());
-
-        sc.close();
     }
 }
